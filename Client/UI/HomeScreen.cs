f65d2b96cafa45671a9cc05693f7bf429c540dff
alumniminium <domi@outlook.at>
2019-07-26 20:47:33 +0200

    added avalonia client template cause console gets too tedious

--- Client/UI/HomeScreen.cs
+++ Client/UI/HomeScreen.cs
@@ -9,7 +9,6 @@ namespace Client.UI
         public static void Draw()
         {
             Console.Clear();
-<<<<<<< HEAD
             var width = Console.WindowWidth;
             var height = Console.WindowHeight;
             var halfwidth = width / 2;
@@ -17,21 +16,6 @@ namespace Client.UI
 
             ConsoleUI.DrawRectangle(0, 0, Console.WindowWidth, Console.WindowHeight);
             ConsoleUI.DrawRectangle(4, 2, 10, 5);
-
-
-=======
-            Console.SetCursorPosition(Console.WindowWidth / 2 -25, 1);
-            Console.WriteLine("|▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒|");
-            Console.SetCursorPosition(Console.WindowWidth / 2 - 25, 2);
-            Console.WriteLine("|           SERVERS      |       FRIENDS          |");
-            Console.SetCursorPosition(Console.WindowWidth / 2 - 25, 3);
-            Console.WriteLine("|_________________________________________________|");
-            for (int i = 0; i < Math.Min(3,serverArray.Length); i++)
-            {
-                Console.SetCursorPosition(Console.WindowWidth / 2 - 25, 4+i);
-                Console.WriteLine($"| 1. {serverArray[0]?.Name?.CenterLength(20)}| 1. {friendArray[0]?.Name?.CenterLength(20)}|");
-            }
->>>>>>> 638be578ec3e61e715fd972057ed899d8e32184a
         }
     }
 }
