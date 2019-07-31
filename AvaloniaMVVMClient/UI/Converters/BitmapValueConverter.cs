11ef9455453b596d06ab203e43cbd1f2808e15a8
Yuki <domi@outlook.at>
2019-07-31 13:57:11 +0200

    Yeah this shit turned into a clusterfuck already.

--- AvaloniaMVVMClient/UI/Converters/BitmapValueConverter.cs
+++ AvaloniaMVVMClient/UI/Converters/BitmapValueConverter.cs
@@ -7,34 +7,23 @@ using Avalonia.Data.Converters;
 
 namespace AvaloniaMVVMClient.UI.Converters
 {
-    public class BitmapValueConverter : IValueConverter
+    public sealed class StringToImageConverter : IValueConverter
     {
-        public static BitmapValueConverter Instance = new BitmapValueConverter();
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType,object parameter, CultureInfo culture)
         {
-            if (value is string && targetType == typeof(IBitmap))
+            try
             {
-                var uri = new Uri((string)value, UriKind.RelativeOrAbsolute);
-                var scheme = uri.IsAbsoluteUri ? uri.Scheme : "file";
-
-                switch (scheme)
-                {
-                    case "file":
-                        return new Bitmap((string)value);
-
-                    default:
-                        var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
-                        return new Bitmap(assets.Open(uri));
-                }
+                return new Bitmap(value.ToString());
             }
-
-            throw new NotSupportedException();
+            catch
+            {
+                return null;
+            }
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object ConvertBack(object value, Type targetType,object parameter, CultureInfo culture)
         {
-            throw new NotSupportedException();
+            throw new NotImplementedException();
         }
     }
 }
