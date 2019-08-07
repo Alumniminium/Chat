38818645eb19adc5b355c16b6cf40ff8fdb28f04
Yuki <domi@outlook.at>
2019-08-07 14:45:44 +0200

    idk what im doing i need help why wont anyone help me

---
+++ AvaloniaMVVMClient/UI/Controls/ChatMessage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaMVVMClient.UI.Controls
+{
+    public class ChatMessage : UserControl
+    {
+        public string AvatarUrl { get; set; }
+        public string Message { get; set; }
+        public ChatMessage()
+        {
+            this.InitializeComponent();
+            DataContext = this;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
--- AvaloniaMVVMClient/UI/Views/HomeView.xaml.cs
+++ AvaloniaMVVMClient/UI/Views/HomeView.xaml.cs
@@ -1,5 +1,9 @@
-﻿using Avalonia.Controls;
+﻿using System.Linq;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using AvaloniaMVVMClient.UI.ViewModels;
+using AvaloniaMVVMClient.UI.Windows;
 
 namespace AvaloniaMVVMClient.UI.Views
 {
@@ -14,6 +18,14 @@ namespace AvaloniaMVVMClient.UI.Views
         {
             AvaloniaXamlLoader.Load(this);
         }
-
+        public async void ServerClick()
+        {
+            var viewModel = (HomeViewModel)MainWindow.Instance.DataContext;
+            
+                await Dispatcher.UIThread.InvokeAsync(() =>
+                {
+                    viewModel.SelectedServer = viewModel.Servers.First();
+                });
+        }
     }
 }
